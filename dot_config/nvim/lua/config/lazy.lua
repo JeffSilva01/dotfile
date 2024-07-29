996961dcb62403a8a1c92b2c611a3ff94e230de3
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-07-28 21:39:44 -0300

    update

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -28,7 +28,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
 
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.octo" },
 
     { import = "lazyvim.plugins.extras.formatting.prettier" },
@@ -36,7 +35,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
